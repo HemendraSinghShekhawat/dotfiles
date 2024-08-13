5578c24e5ed414d42be5446bb054d6e028e80760
HemendraSinghShekhawat <hemustudys@gmail.com>
2024-08-13 09:05:15 +0530

    added new glaze wm config and zebar config

--- nvim/vim-keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Custom code
-vim.o.shell = 'powershell.exe'
--- vim.keymap.set('n', '<C-t>', '<cmd>NeoTree toggle<cr>', { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>ont', '<cmd>NeoTree top<cr>', { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>n', '<cmd>:NERDTreeFocus<CR>')
--- vim.keymap.set('n', '<C-n>', '<cmd>:NERDTree<CR>')
--- vim.keymap.set('n', '<C-t>', '<cmd>:NERDTreeToggle<CR>')
--- vim.keymap.set('n', '<C-f>', '<cmd>:NERDTreeFind<CR>')
